cd40a17ec5a3a5502ba963569f6eec9525a8b261
Heiko W. Rupp <hwr@pilhuhn.de>
2016-07-08 12:21:02 +0200

    Fix regression where URIs as input for the entrypoint no longer worked.

--- spec/unit/base_spec.rb
+++ spec/unit/base_spec.rb
@@ -138,5 +138,21 @@ describe 'Base Spec' do
 
     ret = c.normalize_entrypoint_url 'http://127.0.0.1/hawkular/alerts/', 'hawkular/alerts'
     expect(ret).to eq('http://127.0.0.1/hawkular/alerts')
+
+    ret = c.normalize_entrypoint_url 'https://127.0.0.1/hawkular/alerts/', 'hawkular/alerts'
+    expect(ret).to eq('https://127.0.0.1/hawkular/alerts')
+
+    ret = c.normalize_entrypoint_url 'https://localhost:8080/', 'hawkular/alerts'
+    expect(ret).to eq('https://localhost:8080/hawkular/alerts')
+
+    expect { c.normalize_entrypoint_url 'https://localhost:8080/hawkular/alerts', '' }.to raise_error(ArgumentError)
+
+    uri = URI.parse 'https://localhost:8080/'
+    ret = c.normalize_entrypoint_url uri, 'hawkular/alerts'
+    expect(ret).to eq('https://localhost:8080/hawkular/alerts')
+
+    uri = URI.parse 'http://localhost:8080/hawkular/alerts/'
+    ret = c.normalize_entrypoint_url uri, '/hawkular/alerts/'
+    expect(ret).to eq('http://localhost:8080/hawkular/alerts')
   end
 end
