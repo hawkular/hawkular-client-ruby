548f7b861e8da1f952f300c88be982273b0d5495
jshaughn <jshaughn@localhost.localdomain>
2016-08-24 17:48:09 -0400

    hkwagent-130 The agent now stores op params in the recommended way, update handling

--- lib/hawkular/inventory/entities.rb
+++ lib/hawkular/inventory/entities.rb
@@ -118,13 +118,7 @@ module Hawkular::Inventory
 
     def initialize(op_hash)
       super(op_hash)
-      @params = {}
-      return if op_hash['properties'].nil?
-      param_list = op_hash['properties']['params']
-      return if param_list.nil?
-      param_list.each do |p|
-        @params.store p['name'], p
-      end
+      @params = (op_hash.key? 'parameters') ? op_hash['parameters'] : {}
     end
   end
 
