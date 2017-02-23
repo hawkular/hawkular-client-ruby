d780692106f8e18eea2c1f4ec0af7add1e0e5bba
Josejulio Martínez <jmartine@redhat.com>
2017-02-23 12:57:10 -0600

    Lazy load endpoints.

--- lib/hawkular/hawkular_client.rb
+++ lib/hawkular/hawkular_client.rb
@@ -14,31 +14,15 @@ module Hawkular
       hash[:options] ||= {}
       fail 'no parameter ":entrypoint" given' if hash[:entrypoint].nil?
       @state = hash
-
-      @inventory = Inventory::InventoryClient.create(entrypoint: "#{hash[:entrypoint]}/hawkular/inventory",
-                                                     credentials: hash[:credentials],
-                                                     options: hash[:options])
-
-      @metrics = Metrics::Client.new("#{hash[:entrypoint]}/hawkular/metrics",
-                                     hash[:credentials],
-                                     hash[:options])
-
-      @alerts = Alerts::AlertsClient.new("#{hash[:entrypoint]}/hawkular/alerts",
-                                         hash[:credentials],
-                                         hash[:options])
-
-      @tokens = Token::TokenClient.new(hash[:entrypoint],
-                                       hash[:credentials],
-                                       hash[:options])
     end
 
     def method_missing(name, *args, &block)
       delegate_client = case name
-                        when /^inventory_/ then @inventory
-                        when /^metrics_/ then @metrics
-                        when /^alerts_/ then @alerts
-                        when /^operations_/ then @operations ||= init_operations_client
-                        when /^tokens_/ then @tokens
+                        when /^inventory_/ then inventory
+                        when /^metrics_/ then metrics
+                        when /^alerts_/ then alerts
+                        when /^operations_/ then operations
+                        when /^tokens_/ then tokens
                         else
                           fail "unknown method prefix `#{name}`, allowed prefixes:"\
       '`inventory_`, `metrics_`,`alerts_`,`operations_`, `tokens_`'
@@ -47,11 +31,35 @@ module Hawkular
       delegate_client.__send__(method, *args, &block)
     end
 
+    def inventory
+      @inventory ||= Inventory::InventoryClient.create(entrypoint: "#{@state[:entrypoint]}/hawkular/inventory",
+                                                       credentials: @state[:credentials],
+                                                       options: @state[:options])
+    end
+
+    def metrics
+      @metrics ||= Metrics::Client.new("#{@state[:entrypoint]}/hawkular/metrics",
+                                       @state[:credentials],
+                                       @state[:options])
+    end
+
+    def alerts
+      @alerts ||= Alerts::AlertsClient.new("#{@state[:entrypoint]}/hawkular/alerts",
+                                           @state[:credentials],
+                                           @state[:options])
+    end
+
     # adds a way to explicitly open the new web socket connection (the default is to recycle it)
     # @param open_new [Boolean] if true, opens the new websocket connection
     def operations(open_new = false)
       @operations = init_operations_client if open_new
       @operations ||= init_operations_client
+    end
+
+    def tokens
+      @tokens ||= Token::TokenClient.new(@state[:entrypoint],
+                                       @state[:credentials],
+                                       @state[:options])
     end
 
     private
